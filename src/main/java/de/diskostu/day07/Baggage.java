b482aec65082f21905bdbc4171ee8559ab70984a
Denny Kluge <denny.kluge@gmail.com>
2020-12-09 21:41:19 +0100

    finished day 07 / part 2

--- src/main/java/de/diskostu/day07/Baggage.java
+++ src/main/java/de/diskostu/day07/Baggage.java
@@ -1,12 +1,18 @@
 package de.diskostu.day07;
 
 import lombok.Data;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Baggage {
 
     private final String key;
-    private final List<String> canContain;
+    private List<Pair<Integer, String>> canContain = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return canContain.isEmpty();
+    }
 }
--- src/test/java/de/diskostu/day07/Day07Test.java
+++ src/test/java/de/diskostu/day07/Day07Test.java
@@ -14,15 +14,18 @@ import java.util.stream.Stream;
 @DisplayName("Day 07")
 class Day07Test {
 
-    private static String inputDummy;
-    private static String inputDummy2;
+    private static String inputDummyPart1;
+    private static String inputDummy1Part2;
+    private static String inputDummy2Part2;
+    private static String inputDummy3Part2;
     private static String inputReal;
 
 
     @BeforeAll
     private static void init() {
-        // sum: 7 (slighty differs from website; I adjusted "dotted black bags" which can now contain a shiny gold bag.
-        inputDummy = """
+        // part 1: 7
+        // part 2: 32
+        inputDummyPart1 = """
             light red bags contain 1 bright white bag, 2 muted yellow bags.
             dark orange bags contain 3 bright white bags, 4 muted yellow bags.
             bright white bags contain 1 shiny gold bag.
@@ -33,6 +36,31 @@ class Day07Test {
             faded blue bags contain no other bags.
             dotted black bags contain 1 shiny gold bag.
             """;
+        // part 2: 18
+        inputDummy1Part2 = """
+            shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+            dark olive bags contain 3 faded blue bags.
+            vibrant plum bags contain 6 dotted black bags.
+            faded blue bags contain no other bags.
+            dotted black bags contain no other bags.
+            """;
+        // part 2: 126
+        inputDummy2Part2 = """
+            shiny gold bags contain 2 dark red bags.
+            dark red bags contain 2 dark orange bags.
+            dark orange bags contain 2 dark yellow bags.
+            dark yellow bags contain 2 dark green bags.
+            dark green bags contain 2 dark blue bags.
+            dark blue bags contain 2 dark violet bags.
+            dark violet bags contain no other bags.
+            """;
+        inputDummy3Part2 = """
+            shiny gold bags contain 1 dark olive bag.
+            dark olive bags contain 3 faded blue bags.
+            faded blue bags contain 2 vibrant plum bags, 1 dotted black bag.
+            vibrant plum bags contain 6 dotted black bags.
+            dotted black bags contain no other bags.
+            """;
 
         inputReal = new InputFileSupplier("in/day07.txt").get();
     }
@@ -41,16 +69,35 @@ class Day07Test {
     @ParameterizedTest
     @MethodSource("provideInputAndExpectedResultPart1")
     public void testCalculateSumsPart1(final String input, final int expectedId) {
-        final Day07Part1 sut = new Day07Part1(new InputSupplier(() -> input));
+        final Day07 sut = new Day07(new InputSupplier(() -> input));
 
         Assertions.assertEquals(expectedId, sut.calculateSumPart1());
     }
 
 
+    @ParameterizedTest
+    @MethodSource("provideInputAndExpectedResultPart2")
+    public void testCalculateSumsPart2(final String input, final int expectedId) {
+        final Day07 sut = new Day07(new InputSupplier(() -> input));
+
+        Assertions.assertEquals(expectedId, sut.calculateSumPart2());
+    }
+
+
     private static Stream<Arguments> provideInputAndExpectedResultPart1() {
         return Stream.of(
-            Arguments.of(inputDummy, 7),
+            Arguments.of(inputDummyPart1, 7),
             Arguments.of(inputReal, 378)
+        );
+    }
+
+
+    private static Stream<Arguments> provideInputAndExpectedResultPart2() {
+        return Stream.of(
+            Arguments.of(inputDummy1Part2, 18),
+            Arguments.of(inputDummy2Part2, 126),
+            Arguments.of(inputDummy3Part2, 49),
+            Arguments.of(inputReal, 27526)
         );
     }
 }
